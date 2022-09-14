7b4ece7f9bc4d465da9891b17d9ddfb101df2239
JBerat <berat.99@hotmail.com>
2022-09-14 21:23:51 +0300

    first commit

---
+++ src/pages/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+        <Signup />
+      </div>
+    </Container>
+  );
+}
+
+export default App;
