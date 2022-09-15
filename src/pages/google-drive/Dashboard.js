a43a287a7cceccd821bb6c1d0982e621c6a9caa0
JBerat <berat.99@hotmail.com>
2022-09-15 20:24:45 +0300

    useReducer hook is completed

--- src/pages/google-drive/Dashboard.js
+++ src/pages/google-drive/Dashboard.js
@@ -3,17 +3,31 @@ import Navbar from "./Navbar";
 import { Container } from "react-bootstrap";
 import AddFolderButton from "./AddFolderButton";
 import { useFolder } from "../hooks/useFolder";
+import Folder from "./Folder";
 
 export default function Dashboard() {
-    const { folder } = useFolder("eQlG0AFnLDmcdNute7Hq")
-    
-    console.log(folder)
+  const { folder, childFolders } = useFolder("eQlG0AFnLDmcdNute7Hq");
+
+  console.log(childFolders);
 
   return (
     <>
       <Navbar />
       <Container fluid>
         <AddFolderButton currentFolder={folder} />
+        {childFolders.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFolders.map((childFolder) => (
+              <div
+                key={childFolder.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
       </Container>
     </>
   );
