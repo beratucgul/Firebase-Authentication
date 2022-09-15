1d21440f81e8968dec07be51300d6d011f6c8d7f
JBerat <berat.99@hotmail.com>
2022-09-15 14:30:55 +0300

    password reset email is created

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "../pages/Signup";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import ForgotPassword from "../pages/ForgotPassword";
 import PrivateRoute from "../pages/PrivateRoute";
 
 export default function RoutesComponent() {
@@ -20,6 +21,7 @@ export default function RoutesComponent() {
         />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
