2c145f34eaf02bf159350f8a7c21c96ae6763e07
JBerat <berat.99@hotmail.com>
2022-09-19 17:56:39 +0300

    upload progress component added

--- src/pages/google-drive/AddFileButton.js
+++ src/pages/google-drive/AddFileButton.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileUpload } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../../context/AuthContext";
 import { storage, database } from "../../firebase";
 import { ROOT_FOLDER } from "../hooks/useFolder";
+import { v4 as uuidV4 } from "uuid";
+import { ProgressBar, Toast } from "react-bootstrap";
 
 export default function AddFileButton({ currentFolder }) {
+  const [uploadingFiles, setUploadingFiles] = useState([]);
   const { currentUser } = useAuth();
 
   function handleUpload(e) {
     const file = e.target.files[0];
     if (currentFolder == null || file == null) return;
+
+    const id = uuidV4();
+    setUploadingFiles((prevUplaodingFiles) => [
+      ...prevUplaodingFiles,
+      { id: id, name: file.name, progress: 0, error: false },
+    ]);
 
     const filePath =
       currentFolder === ROOT_FOLDER
@@ -21,31 +31,104 @@ export default function AddFileButton({ currentFolder }) {
       .ref(`/files/${currentUser.uid}/${filePath}`)
       .put(file);
 
-      uploadTask.on("state_changed", snapshot => {
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = snapshot.bytesTransferred / snapshot.totalBytes;
+        setUploadingFiles((prevUplaodingFiles) => {
+          return prevUplaodingFiles.map((uploadFile) => {
+            if (uploadFile.id === id) {
+              return { ...uploadFile, progress: progress };
+            }
 
-      }, () => {
+            return uploadFile;
+          });
+        });
+      },
+      () => {
+        setUploadingFiles((prevUplaodingFiles) => {
+          return prevUplaodingFiles.map((uploadFile) => {
+            if (uploadFile.id === id) {
+              return { ...uploadFile, error: true };
+            }
+            return uploadFile;
+          });
+        });
+      },
+      () => {
+        setUploadingFiles((prevUplaodingFiles) => {
+          return prevUplaodingFiles.filter((uploadFile) => {
+            return uploadFile.id !== id;
+          });
+        });
 
-      }, () => {
-        uploadTask.snapshot.ref.getDownloadURL().then(url => {
-            database.files.add({
-                url: url,
-                name: file.name,
-                createdAt: database.getCurrentTimeStamp(),
-                folderId: currentFolder.id,
-                userId: currentUser.uid,
-            })
-        })
-      })
+        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+          database.files.add({
+            url: url,
+            name: file.name,
+            createdAt: database.getCurrentTimeStamp(),
+            folderId: currentFolder.id,
+            userId: currentUser.uid,
+          });
+        });
+      }
+    );
   }
 
   return (
-    <label className="btn btn-outline-success btn-sm m-0 mr-10">
-      <FontAwesomeIcon icon={faFileUpload} />
-      <input
-        type="file"
-        onChange={handleUpload}
-        style={{ opacity: 0, position: "absolute", left: "-9999px" }}
-      />
-    </label>
+    <>
+      <label className="btn btn-outline-success btn-sm m-0 mr-10">
+        <FontAwesomeIcon icon={faFileUpload} />
+        <input
+          type="file"
+          onChange={handleUpload}
+          style={{ opacity: 0, position: "absolute", left: "-9999px" }}
+        />
+      </label>
+      {uploadingFiles.length > 0 &&
+        ReactDOM.createPortal(
+          <div
+            style={{
+              position: "absolute",
+              bottom: "1rem",
+              right: "1rem",
+              maxWidth: "250px",
+            }}
+          >
+            {uploadingFiles.map((file) => (
+              <Toast
+                key={file.id}
+                onClose={() => {
+                  setUploadingFiles((prevUplaodingFiles) => {
+                    return prevUplaodingFiles.filter((uploadFile) => {
+                      return uploadFile.id !== file.id;
+                    });
+                  });
+                }}
+              >
+                <Toast.Header
+                  closeButton={file.error}
+                  className="text-truncate w-100 d-block"
+                >
+                  {file.name}
+                </Toast.Header>
+                <Toast.Body>
+                  <ProgressBar
+                    animated={!file.error}
+                    variant={file.error ? "danger" : "primary"}
+                    now={file.error ? 100 : file.progress * 100}
+                    label={
+                      file.error
+                        ? "Error"
+                        : `${Math.round(file.progress * 100)}%`
+                    }
+                  />
+                </Toast.Body>
+              </Toast>
+            ))}
+          </div>,
+          document.body
+        )}
+    </>
   );
 }
