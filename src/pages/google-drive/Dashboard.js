c0dfe6fe7a3d3d9ee1a0c1fef106079187d51d56
JBerat <berat.99@hotmail.com>
2022-09-19 16:41:27 +0300

    file adding is done

--- src/pages/google-drive/Dashboard.js
+++ src/pages/google-drive/Dashboard.js
@@ -2,17 +2,17 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Container } from "react-bootstrap";
 import AddFolderButton from "./AddFolderButton";
+import AddFileButton from "./AddFileButton";
 import { useFolder } from "../hooks/useFolder";
 import Folder from "./Folder";
+import File from "./File";
 import { useParams, useLocation } from "react-router-dom";
 import FolderBreadcrumbs from "./FolderBreadcrumbs";
 
 export default function Dashboard() {
   const { folderId } = useParams();
   const { state = {} } = useLocation();
-  const { folder, childFolders } = useFolder(folderId);
-
-  console.log(childFolders);
+  const { folder, childFolders, childFiles } = useFolder(folderId);
 
   return (
     <>
@@ -20,6 +20,7 @@ export default function Dashboard() {
       <Container fluid>
         <div className="d-flex align-items-center">
           <FolderBreadcrumbs currentFolder={folder} />
+          <AddFileButton currentFolder={folder} />
           <AddFolderButton currentFolder={folder} />
         </div>
         {childFolders.length > 0 && (
@@ -31,6 +32,20 @@ export default function Dashboard() {
                 className="p-2"
               >
                 <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
+        {childFolders.length > 0 && childFiles.length > 0 && <hr />}
+        {childFiles.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFolders.map((childFile) => (
+              <div
+                key={childFile.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <File folder={childFile} />
               </div>
             ))}
           </div>
